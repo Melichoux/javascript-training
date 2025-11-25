2e0af220826a1c6fc0d4c88b7206b224cd716f7b
Tournerie Melissa <25093761@afpa.fr>
2025-11-25 16:55:27 +0100

    Exo 5.1 5.2 trtminés

---
+++ exercice.js
@@ -0,0 +1,29 @@
+//  Partie 5.1 -----------------------------------------------------------------------------
+console.log(Partie 5.1);
+
+const store=`Pixel Bay`;
+console.log(store);
+let nb_jeuxinit=500;
+let PU=15;
+console.log(`le stock initial etait de ${nb_jeuxinit} jeux.`) //facon équivalente: console.log("Le stock initial etait de "+nb_jeuxinit+" jeux.");
+let stock = nb_jeuxinit-(nb_jeuxinit*30/100);
+console.log(`Il reste ${stock} jeux en cette fin de journée!`);
+let CA = (nb_jeuxinit*30/100)*PU;
+console.log(`Et le chiffre d'affaire du jour est de ${CA} euro.`);
+
+
+
+//  Partie 5.2 ----------------------------------------------------------------------------
+
+console.log(`Partie 5.2`);
+
+let array_games = [`BF`,`PoP`,`GoW`,`CoD`,`LoL`,`WoW`];
+console.log(array_games);
+console.log(array_games [1]);
+array_games[2] = `DBZ`;
+console.log(array_games [2]);
+
+
+// Partie 5.3 -----------------------------------------------------------------------------
+
+console.log(`Partie 5.3`)
