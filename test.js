ef0585f84b988505890ae0122edda6568efa3822
Tournerie Melissa <25093761@afpa.fr>
2025-11-25 15:03:43 +0100

    Creation exo 5 et install readline-sync

--- test.js
+++ test.js
@@ -15,8 +15,10 @@ lignes */
 
 // console.log(nom);
 
+//  Exemple structure de conditionnelle "if et else" avec imbrication de "else if"
 let age = 18;
-if(typeof age != "number"){
+
+if(typeof age !== "number"){
     console.log("Entrez un age");
 }
 else if (age<0){
@@ -28,4 +30,23 @@ else if (age > 18) {
   console.log("Bon Anniversaire");
 } else {
   console.log("Rentre à pied!");
+}
+
+//  Exemple de boucle "while"
+// let i=0;
+       
+console.log( "Boucle while");
+
+while (i<5)
+{
+    console.log(i);
+    i++;
+}
+
+// Exemple de boucle "for"
+
+console.log( "Boucle for");
+
+for (let i=0; i<5; i++){  //Verifier si declarer la variable dans le for et ne pas la declarer à l'exterieur peut poser probleme
+    console.log(i);
 }
