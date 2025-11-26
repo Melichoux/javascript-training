d532f4323c5ee74407570a93dcf0469634b1596a
Tournerie Melissa <25093761@afpa.fr>
2025-11-26 09:37:05 +0100

    exo 5.5

--- partie5.4.js
+++ partie5.4.js
@@ -5,5 +5,5 @@ console.log(`Partie 5.4`)
 let joursSemaine =[`Lundi`,`Mardi`,`Mercredi`,`Jeudi`,`Vendredi`,`Samedi`,`Dimanche`,]
 
 for (let index = 0; index < joursSemaine.length; index++) {
-    console.log (joursSemaine[index]);
+    console.log (`Jour ${index+1} ${joursSemaine[index]}`);
 }
--- partie5.5.js
+++ partie5.5.js
@@ -1,4 +1,13 @@
 // Partie 5.5 -----------------------------------------------------------------------------
 
-console.log(`Partie 5.5`)
+console.log(`Partie 5.5`);
 
+let stockCible = 100;
+let jeuxEnStock = 0;
+let compteur = 0;
+
+while (jeuxEnStock < stockCible) {
+  jeuxEnStock += 5; // on ajoute 5 jeux au stock à chaque livraison
+  compteur++; // permet de compter le nombre de fois que la boucle tourne ce qui correspond au nombre de livraison
+}
+// console.log(`Il y a eu ${compteur} de livraisons pour completer le stock.`);
