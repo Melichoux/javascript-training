7462be4c2f109c0fae65bec29255f75d94ab6481
Tournerie Melissa <25093761@afpa.fr>
2025-11-26 15:58:18 +0100

    Exo5.7 fini

--- partie5.7.js
+++ partie5.7.js
@@ -1,18 +1,28 @@
 // Partie 5.7 -----------------------------------------------------------------------------
 
+import readlineSync from 'readline-sync';
 console.log(`Partie 5.7`);
 
-let codeSecret = ["Charlie"]
+// let codeSecret = ["Alpha", "Bravo", "Charlie", "Delta"] => FAUX car le parametre sert uniquement a enregistrer les données de l'user
 
-import readlineSync from 'readline-sync';
-const nom = readlineSync.question('Quel est votre nom ? ');
+const codeSecret = readlineSync.question('Quel est le code secret? ');
 
+console.log("avant" + codeSecret)
 
 switch (codeSecret) {
-    case value:
-        
+    case "Alpha":
+        console.log(`Lancer la promotion de la semaine!`)
         break;
-
+    case "Bravo":
+                console.log(`Réorganiser la secton des jeux rétro!`)
+        break;
+    case "Charlie":
+                console.log(`Preparer la zone de démonstration pour les nouveaux arrivages!`)
+        break;
+    case "Delta":
+                console.log(`Vérifier l'inventaire des accessoirese!`)
+        break;
     default:
+                console.log(`Vous ne connaissez pas le code secret! ^-.-^`)
         break;
 }
