ed2640701a93666a9a3f3aead4f427e942246016
Melichoux <meli.tournerie@gmail.com>
2025-12-04 17:10:49 +0100

    Fini partie1 debut partie2

---
+++ partie5.16.js
@@ -0,0 +1,6 @@
+// Partie 5.15 -----------------------------------------------------------------------------
+// import readlineSync from "readline-sync";
+
+console.log(`Partie 5.15`);
+
+//   const anneeUser = readlineSync.question("Quel est l'annee a analyser?")
